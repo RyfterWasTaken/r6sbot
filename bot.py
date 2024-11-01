d568708a823acfc8fc3eea39d0d8e101d0123087
RyfterWasTaken <antoine.f.salaun@gmail.com>
2024-11-01 20:27:18 +0100

    updated

--- bot.py
+++ bot.py
@@ -1,5 +1,7 @@
-import discord
 import os
+import discord
+import r6sapi
+import asyncio
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -7,15 +9,7 @@ token = os.getenv("TOKEN")
 
 bot = discord.Bot()
 
-greetings = bot.create_group("greetings", "Greet people")
 
-@greetings.command()
-async def hello(ctx):
-  await ctx.respond(f"Hello, {ctx.author}!")
-
-@greetings.command()
-async def bye(ctx):
-  await ctx.respond(f"Bye, {ctx.author}!")
 
 bot.run(token)
 
